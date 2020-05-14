72511d013e30a51768922b26f98ca05c85524276
Thomas Chaplin <tomrchaplin@gmail.com>
2020-05-14 14:48:30 +0100

    Ignore html extensions on host

--- host.js
+++ host.js
@@ -3,5 +3,5 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static(path.join(__dirname,'dist')));
+app.use(express.static(path.join(__dirname,'dist'), {index: false, extensions:['html']}));
 app.listen(NODE_PORT, () => console.log(`Server started on port ${NODE_PORT}`));
